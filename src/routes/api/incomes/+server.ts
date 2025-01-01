4206342ea5dd7a1aaacb5f61211fc548aa0bc76a
Ryan Arjoon <ryanarjoon4@gmail.com>
2025-01-01 12:16:50 -0500

    added post to income api

--- src/routes/api/incomes/+server.ts
+++ src/routes/api/incomes/+server.ts
@@ -1,4 +1,7 @@
-import { json } from '@sveltejs/kit';
+import { json, type RequestEvent } from '@sveltejs/kit';
+import { prisma } from '$lib/prisma';
+import { authenticate } from '$lib/auth/jwt.js';
+
 export function GET() {
 	const responseBody = {
 		status: 'success',
@@ -6,4 +9,31 @@ export function GET() {
 	};
 
 	return json(responseBody);
+}
+
+export async function POST({ request }: RequestEvent) {
+	const user = await authenticate(request);
+
+	if (user instanceof Response) {
+		return user;
+	}
+
+	try {
+		const { name, description, amount, account, type } = await request.json();
+
+		if (!name || !amount || !type) {
+			return json(
+				{ status: 'error', message: 'Please enter the required fields' },
+				{ status: 400 }
+			);
+		}
+
+		const newIncome = await prisma.income.create({
+			data: { name, description, amount, account, userId: user.id, type }
+		});
+
+		return json(newIncome, { status: 201 });
+	} catch (error: any) {
+		return json({ status: 'error', message: error.message }, { status: 500 });
+	}
 }
