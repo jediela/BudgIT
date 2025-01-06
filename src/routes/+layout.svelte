74b328bf40d7a4ff3a3b9b38ac909b6be5d8cfae
Jed Antalan <jediel2424@gmail.com>
2025-01-05 22:36:47 -0500

    add mode watcher for dark mode theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import '../app.css';
+	import { ModeWatcher } from "mode-watcher";
 	let { children } = $props();
 </script>
 
+<ModeWatcher />
 <div>ROOT LAYOUT</div>
 {@render children()}
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,27 +1,47 @@
+<!-- <script lang="ts">
+	import { goto } from '$app/navigation';
+
+	let email = $state('');
+	let password = $state('');
+
+	async function handleLogin(event: Event) {
+		event.preventDefault();
+
+		try {
+			const res = await fetch('/api/users/login', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({ email, password })
+			});
+
+			const data = await res.json();
+
+			goto('/dashboard');
+
+			if (data.status === 'error') {
+				throw new Error(data.message);
+			}
+		} catch (error: any) {
+			throw new Error(error.message);
+		}
+	}
+</script>
+
+<div>login</div>
+<form onsubmit={handleLogin}>
+	<input placeholder="email" bind:value={email} type="text" />
+	<input placeholder="password" bind:value={password} type="text" />
+	<button type="submit">submit</button>
+</form> -->
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button/index.js';
 	import * as Card from '$lib/components/ui/card/index.js';
 	import { Input } from '$lib/components/ui/input/index.js';
 	import { Label } from '$lib/components/ui/label/index.js';
 
-	const frameworks = [
-		{
-			value: 'sveltekit',
-			label: 'SvelteKit'
-		},
-		{
-			value: 'next',
-			label: 'Next.js'
-		},
-		{
-			value: 'astro',
-			label: 'Astro'
-		},
-		{
-			value: 'nuxt',
-			label: 'Nuxt.js'
-		}
-	];
+	
 </script>
 
 <Card.Root class="w-[350px]">
